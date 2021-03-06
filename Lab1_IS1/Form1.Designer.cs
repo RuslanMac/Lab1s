7d01d4f8e38be653171c0d05b4f197f885814a01
Ruslan <ruslan-karamurzanov@mail.ru>
2021-03-06 11:14:34 +0700

    Добавьте файлы проекта.

---
+++ Lab1_IS1/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace Lab1_IS1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(56, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(185, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Введите текст пожалуйста";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(59, 70);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(182, 57);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(59, 200);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(90, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Шифротекст";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(62, 238);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(66, 17);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Text New";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(306, 104);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(119, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Выполнить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(376, 200);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(117, 17);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Исходный текст ";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(379, 238);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(46, 17);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "label5";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(62, 294);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(137, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Преобразить ";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Lab1_IS1/Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab1_IS1
+{
+    public partial class Form1 : Form
+    {
+        public char[,] symbols;
+
+        public string str;
+
+        public Dictionary<char, int> table;
+
+        
+        public Form1()
+        {
+            InitializeComponent();
+
+            symbols = new char[8,9] { 
+                { 'а' ,'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з'},
+                        { 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р'  },  
+                            { 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч',   'ш', 'щ' }, 
+                            { 'ъ', 'ы', 'ь', 'э', 'ю', 'я', 'А', 'Б', 'В' },
+                            { 'Г', 'Д', 'Е', 'Ё', 'Ж', 'З', 'И', 'Й', 'К'},
+                            { 'Л', 'М', 'Н', 'О', 'П', 'Р', 'С', 'Т', 'У'},
+                            { 'Ф', 'Х', 'Ц', 'Ч',   'Ш',  'Щ', 'Ъ', 'Ы', 'Ь'  },
+                            { 'Э' ,  'Ю', 'Я',  ' ', '.', ':', '!', '?', ',' }
+            };
+
+
+
+
+        }
+
+     
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        public string ConvertMessage(string str)
+        {
+            string str1 = "";
+            
+            for(int i=0; i<str.Length; i++)
+            {
+                str1 += FindValue(str[i]); 
+            }
+
+            return str1;
+        }
+
+        public string ConvertNewMessage(string str1)
+        {
+            string message = "";
+            string strCopy = str1;
+            int i = 0;
+            int j = 0;
+            while (strCopy != "")
+            {
+                i = Convert.ToInt32(strCopy.Substring(0, 1));
+                j = Convert.ToInt32(strCopy.Substring(1, 1));
+                message = message + FindOrder(i, j);
+                strCopy = strCopy.Substring(2);
+            }
+            return message;
+            
+           
+        }
+
+        public string FindValue(char symbolKey)
+        {
+            int i = 0;
+            int j = 0;
+            while (i < 8 && j < 9 && symbols[i,j] != symbolKey && i+j<17)
+            {
+                j = j+1;
+                if (j > 8)
+                {
+                    j = 0;
+                    i = i +  1;
+                }
+              
+            }
+            return i.ToString() + j.ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            str = this.textBox1.Text;
+            string str1;
+            string message;
+            try
+            {
+                str1 = ConvertMessage(str);
+                this.label3.Text = str1;
+                this.label5.Text = "";
+                message = ConvertNewMessage(str1);
+                this.label5.Text = message;
+            }
+            catch (Exception ex)
+            {
+                this.textBox1.Text = "ОШИБКА!!! Ввод не ясен, попробуйте заново.";
+                this.label3.Text = "";
+                this.label5.Text = "";
+               
+            }
+
+
+        }
+
+        public string FindOrder(int i, int j )
+        {
+            return symbols[i, j].ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
